cb7b87ba7a217012776467171cf2543c665cdaea
Talha Ansari <talhaansari1606@gmail.com>
2024-03-24 00:29:00 +0530

    added mongo

--- main.py
+++ main.py
@@ -4,40 +4,15 @@ from pymongo import MongoClient
 from datetime import datetime
 
 client = MongoClient(
-    "mongodb+srv://admin:OvpIVRbKRSH92ZQW@cluster0.hymysuv.mongodb.net/")
+    "MONGODB_URL")
 
-client_id = '071feac917ced9c'
-client_secret = 'cff15a70650188c9da7525312034a087a6c1a4ec'
-referesh_token = '8c83a1ec729ba7bb0e466a4e6086653abf42cd7b'
-access_token = 'c35a9290605ddde61edc26f0b50c6cff188eb13d'
+
+access_token = 'ACCESS_TOKEN'
 
 db = client.frames
 images = db.images_links
 
 
-def create_album(access_token):
-    url = "https://api.imgur.com/3/album"
-
-    payload = {
-        'title': 'AUV Album',
-        'description': 'This albums contains images coming from ROV.',
-        'cover': 'MwDV3S5'}
-    files = [
-
-    ]
-    headers = {
-        'Authorization': f'Bearer {access_token}',
-    }
-
-    response = requests.request(
-        "POST", url, headers=headers, data=payload, files=files)
-
-    album_data = json.loads(response.text)
-    print(album_data)
-    album_id = json.loads(response.text)['data']['id']
-    album_deletehash = json.loads(response.text)['data']['deletehash']
-    print("album created", album_id)
-    return album_data
 
 
 def upload_image(access_token, image_path):
@@ -67,40 +42,6 @@ def upload_image(access_token, image_path):
     return image_link
 
 
-def add_image_to_album(image_deletehash, album_deletehash):
-    url = f'https://api.imgur.com/3/album/{album_deletehash}/add'
-
-    payload = {'deletehashes[]': f'{image_deletehash}'
-               }
-    files = [
-
-    ]
-    headers = {
-        'Authorization': f'Bearer {access_token}',
-
-    }
-
-    response = requests.request(
-        "POST", url, headers=headers, data=payload, files=files)
-
-    print(response.text)
-
-
-def get_album_images(access_token, album_id):
-    url = f"https://api.imgur.com/3/album/{album_id}/images"
-    payload = {}
-    files = [
-
-    ]
-    headers = {
-        'Authorization': f'Bearer {access_token}',
-    }
-
-    response = requests.request(
-        "GET", url, headers=headers, data=payload, files=files)
-
-    print(response.text)
-    return json.loads(response.text)
 
 
 if __name__ == "__main__":
